1570a7d85559915baf81e1206ec7f209b6ccd7d3
ainga-ri <alfred.inga.rios@gmail.com>
2022-09-05 11:25:24 +0200

    class Name must be equal to archive name

--- javaFundamentals/Strings/substringComparisions.java
+++ javaFundamentals/Strings/substringComparisions.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class Solution {
+public class substringComparisions {
 
     public static String getSmallestAndLargest(String s, int k) {
         String smallest = "";
